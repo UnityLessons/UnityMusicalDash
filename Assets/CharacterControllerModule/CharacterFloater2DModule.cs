0fceda0ace05429cdb06eaa4b0251ac2b880d9b4
Helpfully <ztolgakacar@gmail.com>
2015-02-09 22:40:26 +0200

    Started developing CharacterFloater2DModule

--- Assets/CharacterControllerModule/CharacterFloater2DModule.cs
+++ Assets/CharacterControllerModule/CharacterFloater2DModule.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 [RequireComponent(typeof(SimpleJumpControl2DModule))]
 [AddComponentMenu("Modular Character Controls/Character Floater 2D Module")]
-public class CharacterFloater2DModule : CharacterController2DModule 
+public class CharacterFloater2DModule : CharacterController2DModule
 {
     public bool isGrounded
     {
@@ -13,12 +13,12 @@ public class CharacterFloater2DModule : CharacterController2DModule
         }
     }
 
-    private bool _canIFloat = true;
+    private bool isFloatUsed = false;
     public bool canIFloat
     {
         get
         {
-            return _canIFloat && !isGrounded;
+            return !isFloatUsed && !isGrounded;
         }
     }
 
@@ -30,16 +30,6 @@ public class CharacterFloater2DModule : CharacterController2DModule
         }
     }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
     public override void Init()
     {
         Debug.Log("CharacterFloater2DModule Initialized!!");
@@ -47,6 +37,24 @@ public class CharacterFloater2DModule : CharacterController2DModule
 
     public override void Control()
     {
-        
+
+    }
+
+    private void FloatControl()
+    {
+        if (canIFloat && doIWantToFloat)
+        {
+            Float();
+        }
+    }
+
+    private void Float()
+    {
+
+    }
+
+    private void FloatResetControl()
+    {
+
     }
 }
